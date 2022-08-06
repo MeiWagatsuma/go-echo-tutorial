92495beea74f76926ada9ba8442fc75cc9221112
4ttoE <attoe255@gmail.com>
2022-08-06 12:51:06 +0900

    Retrieve and display query parameters

--- src/main/main.go
+++ src/main/main.go
@@ -11,11 +11,24 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello from the web side!")
 }
 
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	return c.String(
+		http.StatusOK,
+		fmt.Sprintf(
+			"your cat name is: %s\nnand his type is: %s\n",
+			catName,
+			catType))
+}
+
 func main() {
 	fmt.Println("Welcome to the server")
 
 	e := echo.New()
 	e.GET("/", hello)
+	e.GET("/cats", getCats)
 
 	e.Start(":8080")
 }
